6885e2103bdb9df1952ec86241e8ff97cde784ba
Osmany González <devbiitz2@gmail.com>
2020-06-12 09:29:35 +0100

    Added /addwhitelist steamId (CLIENT/CONSOLE)

--- vorpcore_sv/Scripts/Commands.cs
+++ vorpcore_sv/Scripts/Commands.cs
@@ -81,6 +81,50 @@ namespace vorpcore_sv.Scripts
                     Debug.WriteLine("This only can be executed from client side.");
                 }
             }), false);
+
+            TriggerEvent("chat:addSuggestion", "/addwhitelist", "Example: /addwhitelist 11000010c8aa16e");
+            API.RegisterCommand("addwhitelist", new Action<int, List<object>, string>((source, args, rawCommand) =>
+            {
+                if (source > 0) // it's a player.
+                {
+                    Player _source = Database.getSource(source);
+                    TriggerEvent("vorp:getCharacter", source, new Action<dynamic>((user) =>
+                    {
+                        if (user.group == "admin")
+                        {
+                            try
+                            {
+                                string steamId = args[0].ToString();
+                                Exports["ghmattimysql"].execute("SELECT * FROM whitelist WHERE identifier = ?", new[] { steamId }, new Action<dynamic>((result) =>
+                                {
+                                    if (result.Count == 0)
+                                    {
+                                        Exports["ghmattimysql"].execute("INSERT INTO whitelist (`identifier`) VALUES (?)", new object[] { steamId });
+                                        Whitelist.whitelist.Add(steamId);
+                                        _source.TriggerEvent("vorp:Tip", $"Added {steamId} to whitelist", 4000);
+                                    }
+                                    else
+                                    {
+                                        _source.TriggerEvent("vorp:Tip", $"{steamId} Is Whitelisted {steamId}", 4000);
+                                    }
+                                }));
+                            }
+                            catch
+                            {
+                                _source.TriggerEvent("vorp:Tip", "ERROR: Use Correct Sintaxis", 4000);
+                            }
+                        }
+                        else
+                        {
+                            _source.TriggerEvent("vorp:Tip", LoadConfig.Langs["NoPermissions"], 4000);
+                        }
+                    }));
+                }
+                else
+                {
+                    Debug.WriteLine("This only can be executed from client side.");
+                }
+            }), false);
         }
     }
 }
