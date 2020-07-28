14f1adc246f5af0da0f5c773e35a22a7ed473eaa
Osmany González <devbiitz2@gmail.com>
2020-07-28 04:05:36 +0100

    VoiceChat native added

---
+++ vorpcore_cl/Scripts/VoiceChat.cs
@@ -0,0 +1,57 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace vorpcore_cl.Scripts
+{
+    class VoiceChat : BaseScript
+    {
+        public static bool activeVoiceChat = false;
+        public static List<float> voiceRange = new List<float>();
+        public static int voiceRangeSelected = 0;
+
+        public static uint keyRange = 0;
+
+        //Tecla L
+        public VoiceChat()
+        {
+            Tick += SetVoiceChat;
+            Tick += StartVoiceChat;
+        }
+
+        private async Task StartVoiceChat()
+        {
+            if (Utils.GetConfig.isLoading && activeVoiceChat)
+            {
+                Function.Call((Hash)0x08797A8C03868CB8, voiceRange[voiceRangeSelected]);
+                Function.Call((Hash)0xEC8703E4536A9952);
+                Function.Call((Hash)0x58125B691F6827D5, voiceRange[voiceRangeSelected]);
+            }
+            await Delay(10000);
+        }
+
+        private async Task SetVoiceChat()
+        {
+            if (Utils.GetConfig.isLoading && activeVoiceChat)
+            {
+                if (API.IsControlJustPressed(0, keyRange))
+                {
+                    Debug.WriteLine(keyRange.ToString());
+                    voiceRangeSelected = (voiceRangeSelected + 1) % voiceRange.Count;
+                    TriggerEvent("vorp:TipRight", string.Format(Utils.GetConfig.Langs["VoiceRangeChanged"], voiceRange[voiceRangeSelected].ToString()), 4000);
+                    Function.Call((Hash)0x08797A8C03868CB8, voiceRange[voiceRangeSelected]);
+                    Function.Call((Hash)0xEC8703E4536A9952);
+                    Function.Call((Hash)0x58125B691F6827D5, voiceRange[voiceRangeSelected]);
+                }
+                if (API.IsControlPressed(0, keyRange))
+                {
+                    Vector3 playerCoords = API.GetEntityCoords(API.PlayerPedId(), true, true);
+                    Function.Call((Hash)0x2A32FAA57B937173, 0x94FDAE17, playerCoords.X, playerCoords.Y, playerCoords.Z - 0.5f, 0.0f, 0.0f, 0.0f, 0, 0.0f, 0.0f, voiceRange[voiceRangeSelected], voiceRange[voiceRangeSelected], 1.0, 255, 179, 38, 200, false, true, 2, false, false, false, false);
+                }
+            }
+        }
+    }
+}
--- vorpcore_cl/Utils/GetConfig.cs
+++ vorpcore_cl/Utils/GetConfig.cs
@@ -13,6 +13,8 @@ namespace vorpcore_cl.Utils
         public static JObject Config = new JObject();
         public static Dictionary<string, string> Langs = new Dictionary<string, string>();
 
+        public static bool isLoading = false;
+
         public GetConfig()
         {
             EventHandlers[$"{API.GetCurrentResourceName()}:SendConfig"] += new Action<string, ExpandoObject>(LoadDefaultConfig);
@@ -37,9 +39,23 @@ namespace vorpcore_cl.Utils
             Scripts.DiscRichPresence.drp_active = Config["ActiveDRP"].ToObject<bool>();
             Scripts.IDHeads.UseIDHeads = Config["HeadId"].ToObject<bool>();
             Scripts.IDHeads.UseKeyMode = Config["ModeKey"].ToObject<bool>();
-
+            Scripts.VoiceChat.activeVoiceChat = Config["ActiveVoiceChat"].ToObject<bool>();
             Scripts.IDHeads.keyShow = FromHex(Config["KeyShowIds"].ToString());
 
+            Scripts.VoiceChat.keyRange = FromHex(Config["KeySwapVoiceRange"].ToString());
+
+            float voiceRangeDefault = Config["DefaultVoiceRange"].ToObject<float>();
+            foreach (var r in Config["VoiceRanges"])
+            {
+                Scripts.VoiceChat.voiceRange.Add(r.ToObject<float>());
+            }
+
+            if (Scripts.VoiceChat.voiceRange.IndexOf(voiceRangeDefault) != -1)
+            {
+                Scripts.VoiceChat.voiceRangeSelected = Scripts.VoiceChat.voiceRange.IndexOf(voiceRangeDefault);
+            }
+
+            isLoading = true;
         }
 
         public static uint FromHex(string value)
